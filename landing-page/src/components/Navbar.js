a74c2d171011752f440a99f279df3e346e829120
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-04 11:58:49 +0530

    Update Navbar.js

--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
                     <span className='navAbout'>About</span>
                     <span className='navStore'>Locate Store</span> 
                                 
-                    <span style={{marginRight: "3vh", display: "inline-block"}}>Scooter  <a href="#" onClick={ () => setClicked(!clicked) }><span className='mediaArrow' ></span></a></span>
+                    <span style={{marginRight: "3vh", display: "inline-block"}}>Scooter  <span onClick={ () => setClicked(!clicked) }><span className='mediaArrow' ></span></span></span>
 
                     <span style={{marginRight: "3vh", display: "inline-block"}}>Purchase Assistance  <span className='mediaArrow'></span></span>
                     <button className="btnDealer">Dealership</button>
